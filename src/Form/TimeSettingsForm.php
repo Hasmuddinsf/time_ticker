dfc2defc355229b9870d516761da0fc56ebe2e2a
Hasmuddin <hasmuddinsf@gmail.com>
2022-09-28 20:52:34 +0530

    Added configuration form with required fields

---
+++ src/Form/TimeSettingsForm.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @file
+ * Contains the settings for administering the Timezone
+ */
+
+namespace Drupal\time_ticker\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class TimeSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'time_ticker_admin_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'time_ticker.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $config = $this->config('time_ticker.settings');
+    $form['country'] = [
+      '#type' => 'textfield',
+      '#required' => TRUE,
+      '#title' => $this->t('Country'),
+      '#default_value' => $config->get('country'),
+      '#description' => $this->t('Please enter the Country name.'),
+    ];
+    $form['city'] = [
+      '#type' => 'textfield',
+      '#required' => TRUE,
+      '#title' => $this->t('City'),
+      '#default_value' => $config->get('city'),
+      '#description' => $this->t('Please enter the City name.'),
+    ];
+    $form['timezone'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Timezone'),
+      '#required' => TRUE,
+      '#options' => [
+        'America/Chicago' => $this->t('America/Chicago'),
+        'America/New_York' => $this->t('America/New_York'),
+        'Asia/Tokyo' => $this->t('Asia/Tokyo'),
+        'Asia/Dubai' => $this->t('Asia/Dubai'),
+        'Asia/Kolkata' => $this->t('Asia/Kolkata'),
+        'Europe/Amsterdam' => $this->t('Europe/Amsterdam'),
+        'Europe/Oslo' => $this->t('Europe/Oslo'),
+        'Europe/London' => $this->t('Europe/London'),
+      ],
+      '#default_value' => $config->get('timezone'),
+      
+    ];
+    
+  
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $country = $form_state->getValue('country');
+    $city = $form_state->getValue('city');
+    $timezone = $form_state->getValue('timezone');
+    
+    $config = $this->config('time_ticker.settings');
+    $config->set('country', $country);
+    $config->set('city', $city);
+    $config->set('timezone', $timezone);
+    $config->save();
+
+    parent::submitForm($form, $form_state);
+  }
+}
